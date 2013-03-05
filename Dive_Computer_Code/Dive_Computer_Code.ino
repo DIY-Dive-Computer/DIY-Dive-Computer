9e4c5ef4ff747b419499f1aa365ef0af1cb0f606
Victor <vickonshin@mac.com>
2013-03-05 18:27:43 -0500

    The beginnings of an interactive user interface.

--- Dive_Computer_Code/Dive_Computer_Code.ino
+++ Dive_Computer_Code/Dive_Computer_Code.ino
@@ -11,42 +11,145 @@
 #include <Adafruit_SSD1306.h>
 #include <SD.h>
 #include "RTClib.h"
+#include "PITimer.h"
 
 #define OLED_RESET 3 // OLED Reset pin
-#define HEADER_DATA_GAP 9
-//#define SHOW_LAYOUT
+#define HEADER_DATA_GAP 9 // The gap between the heading line and the data line on most display items.
+//#define SHOW_LAYOUT // Uncomment this to see bounding boxes on UI elements - makes laying out items easier.
 
 Adafruit_SSD1306 display(OLED_RESET); // Allocate for the OLED
 
-uint32_t diveStart = 0; // This stores the dive start time in unixtime.
-int diveTime = 0;// This stores the current dive time in seconds
 // Dive parameter storage
-int depth = 0;
-int maxDepth = 0;
-int nitrogen = 0;
-int oxygen = 10;
+uint32_t  diveStart            = 0; // This stores the dive start time in unixtime.
+int       diveTime             = 0; // This stores the current dive time in seconds
+int       depth                = 0; // Current Depth
+int       maxDepth             = 0; // Max Depth
+int       nitrogen             = 0; // Current nitrogen saturation level
+int       oxygen               = 0; // Current oxygen saturation level.
+int       temperature          = 0; // Current temp.
+int       batteryLevel         = 128; // Current battery capacity remaining.
+int       displayMode          = 0; // Stores the current display mode: 0 = menu, 1 = dive, 2 = log, 3 = settings. 
+int       currentMenuOption    = 0; // Stores which menu option is being displayed. 0 = dive, 1 = log, 2 = settings.
 
-uint32_t lastDataRecord = 0; // Stores the last time data was recorded in unixtime.
-uint32_t lastColonStateChange = 0; // Stores the last time the clock was updates to that the blink will happen at 1Hz.
+boolean   diveMode             = false; // Stores dive mode or surface mode state: 0 = surface mode, 1 = dive mode.
+boolean   alertShowing         = false; // true = there is an alert showing. 
+uint32_t  alertTime            = 0; // Stores time the alert was displayed.
+uint32_t  lastDataRecord       = 0; // Stores the last time data was recorded in unixtime.
+uint32_t  lastColonStateChange = 0; // Stores the last time the clock was updates to that the blink will happen at 1Hz.
 
-DateTime now; // Stores the current date/time for use throughout the code.
-const int chipSelect = 4; // For the SD card breakout board
+DateTime  now;                      // Stores the current date/time for use throughout the code.
+const int chipSelect           = 4; // For the SD card breakout board
 
 // These will be user preferences
-boolean time12Hour = true;
-boolean clockBlink = true;
-int recordInterval = 29; // This is the inteval in which it will record data to the SD card minus one.
+boolean   time12Hour           = true; // true = 12 hour time, false = 24 hour time.
+boolean   clockBlink           = true; // true = blink the colon, false = don't blink the colon.
+boolean   ferinheight          = true; // true = Ferinheight, false = Celsius.
+
+int       recordInterval       = 29; // This is the inteval in which it will record data to the SD card minus one.
+
+static unsigned char PROGMEM dive_logo_bmp[] =
+{ 
+  B11111111, B11111111, B11111111,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B10000000, B00000001,
+  B10000000, B10000000, B00000001,
+  B10000000, B10000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B11111111, B11111111, B11111111 };
+  
+  static unsigned char PROGMEM log_logo_bmp[] =
+{ 
+  B11111111, B11111111, B11111111,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B10100000, B00000001,
+  B10000000, B10100000, B00000001,
+  B10000000, B10100000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B11111111, B11111111, B11111111 };
+
+  static unsigned char PROGMEM settings_logo_bmp[] =
+{ 
+  B11111111, B11111111, B11111111,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B10101000, B00000001,
+  B10000000, B10101000, B00000001,
+  B10000000, B10101000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B10000000, B00000000, B00000001,
+  B11111111, B11111111, B11111111 };
 
 // *******************************************************************
 // ***************************** Setup *******************************
 // *******************************************************************
 
 void setup()   {                
-  Serial.begin(9600);
+  Serial.begin(115200);
   Wire.begin();
 
-  // Make sure the real time clock is set
+  // Display Setup
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D
+  display.display(); // show splashscreen
+  delay(2000);
+  display.clearDisplay();
+  display.display(); 
+  display.setTextColor(WHITE);
+
+  // Make sure the real time clock is set correctly and set some inital parameters
   Teensy3Clock.set(DateTime(__DATE__, __TIME__).unixtime());
+  now = Teensy3Clock.get();
+  lastDataRecord = Teensy3Clock.get();
+  lastColonStateChange = lastDataRecord;
+  diveStart = now.unixtime();
 
   // initialize access to the SD card
   Serial.print("Initializing SD card...");
@@ -54,47 +157,143 @@ void setup()   {
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
 
+  //testing
+  //showAlert (0, "You are about to     become bent and die! Time to surface!");
+  temperature = 75;
+
+  //  Setup the user input buttons.
+  pinMode(5,INPUT); // right button
+  pinMode(6,INPUT); // left button
+  attachInterrupt(5, rightPressed, FALLING);
+  attachInterrupt(6, leftPressed, FALLING);
+
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
-    return;
+    showAlert (0, "Failed to initialize the SD card.  Please make sure the card isinstalled correctly.");
+  } 
+  else {
+    Serial.println("card initialized.");
   }
-  Serial.println("card initialized.");
 
-  now = Teensy3Clock.get();
-  lastDataRecord = Teensy3Clock.get();
-  lastColonStateChange = lastDataRecord;
-
-  diveStart = now.unixtime();
-
-  // Display Setup
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D
-  display.clearDisplay();
-  display.setTextColor(WHITE);
 
   // Give everything a half sec to settle
   delay(500);
+}
+
+// *******************************************************************
+// *******************************************************************
+// **************************** Main Loop ****************************
+// *******************************************************************
+// *******************************************************************
+
+void loop() {
+
+  now = DateTime(Teensy3Clock.get());
+  diveTime = now.unixtime() - diveStart;
+  depth = analogRead(A0) / 5;
+
+  display.clearDisplay();
+
+  if (!alertShowing) {
+    // 0 = menu, 1 = dive, 2 = log, 3 = settings
+    if (displayMode == 0) {
+      // Menu Display
+      drawMenu();
+    } 
+    else if (displayMode == 1) {
+      // Dive Display
+      drawDiveDiveDiplayA();
+    }
+    else if (displayMode == 2) {
+      // Log Book Display
+    }
+
+    else if (displayMode == 3) {
+      // Settings Display
+    }
+  }
+  display.display();
+}
+
+void drawMenu() {
+  display.setTextColor(WHITE);
+
+  drawClock(0, 0, 1, false); // Parameters: x, y, size, bold
+  drawTemp(97, 0, 1, false);
+  drawBattery(50, 5);
+
+  display.setTextSize(1);
+  if (currentMenuOption == 0) {
+    display.setCursor( 37, 56 );      
+    display.println("Dive Mode");
+      display.drawBitmap(52, 22, dive_logo_bmp, 24, 24, WHITE);
+
+
+  } 
+  else if (currentMenuOption == 1) {
+    display.setCursor( 40, 56 );      
+    display.println("View Log");
+      display.drawBitmap(52, 22, log_logo_bmp, 24, 24, WHITE);
+
+  } 
+  else if (currentMenuOption == 2) {
+    display.setCursor( 40, 56 );      
+    display.println("Settings");
+      display.drawBitmap(52, 22, settings_logo_bmp, 24, 24, WHITE);
+
+  }
+
+  // TODO: Draw Dive Icon
+
+  display.fillRect(115, 55, 13, 9, WHITE);
+  display.setTextColor(BLACK);
+  display.setCursor( 116, 56 );
+  display.setTextSize(1);
+  display.println(">>");
+
+  display.fillRect(0, 55, 19, 9, WHITE);
+  display.setTextColor(BLACK);
+  display.setCursor( 1, 56 );
+  display.setTextSize(1);
+  display.println("SEL");
 
 
 }
 
+
 // *******************************************************************
-// **************************** Main Loop ****************************
+// ************************ User Interactions ************************
 // *******************************************************************
 
+void rightPressed() {
+  if (alertShowing) {
+    killAlert();
+    return;
+  }
+  if (displayMode == 0) { // Menu
+    currentMenuOption ++;
+    if (currentMenuOption > 2) currentMenuOption = 0;
+  }
+}
 
-void loop() {
-  now = DateTime(Teensy3Clock.get());
-  diveTime = now.unixtime() - diveStart;
+void leftPressed() {
 
-  //  ************* Display code *************
+  if (displayMode == 0) { // Menu
+    displayMode = currentMenuOption + 1;
+  }
 
-  display.clearDisplay();
+}
 
-  depth = analogRead(A0) / 4;
+// *******************************************************************
+// *********************** Dive Mode Displays ************************
+// *******************************************************************
+
+void drawDiveDiveDiplayA() {
 
   if (depth > maxDepth) maxDepth = depth;
+  display.setTextColor(WHITE);
 
   //drawDepth( 0, 32, 3, false );
   //drawClock( 0, 0, 2, true);
@@ -104,13 +303,32 @@ void loop() {
   drawDepth( depth, 6, 0, 3, false, "Depth:"); // Parameters: depth, x, y, size, bold, header string
   drawSaturation(nitrogen, true);
   drawSaturation(oxygen, false);
+  drawTemp(61, 42, 1, false);
+
+  display.fillRect(115, 55, 13, 9, WHITE);
+  display.setTextColor(BLACK);
+  display.setCursor( 116, 56 );
+  display.setTextSize(1);
+  display.println(">>");
+
+  display.fillRect(0, 55, 13, 9, WHITE);
+  display.setTextColor(BLACK);
+  display.setCursor( 1, 56 );
+  display.setTextSize(1);
+  display.println("<<");
 
   nitrogen++;
   oxygen++;
 
+  logData();
 
-  display.display();
+}
 
+// *******************************************************************
+// ************************** Record Data ****************************
+// *******************************************************************
+
+void logData() {
   //  ************* Data logging code *************
 
   if ( now.unixtime() > (lastDataRecord + recordInterval) ) {
@@ -134,19 +352,16 @@ void loop() {
       Serial.println("error opening datalog.txt");
     } 
   }
-
 }
-
-// *******************************************************************
-// ********************** Generate Record Data ***********************
-// *******************************************************************
 
 String generateRecordDataString() {
 
-  String returnString = "";
-
+  String returnString = "Nitrogen: ";
+  returnString += String (nitrogen);
   return returnString;  
 }
+
+
 
 // *******************************************************************
 // ************************ Draw Saturation **************************
@@ -294,6 +509,69 @@ void drawClock(int x, int y, int size, boolean bold) {
 }
 
 // *******************************************************************
+// *************************** Draw Temp ****************************
+// *******************************************************************
+
+// Good up to three digits. Sizes 1-4 are valid
+void drawTemp(int x, int y, int size, boolean bold) {
+
+  int headerLength = 5; // "Temp:"
+  int headerWidth = ( 6 * headerLength);
+
+  int width = ( ( 6 * 4 ) * size) - size + 1;
+  int height = (7 * size) + HEADER_DATA_GAP;
+
+  if ( width < headerWidth ) width = headerWidth;
+
+  int tempWidth = 10 * size;
+
+  if (temperature > 9) {
+    tempWidth = 16 * size;
+  }
+  if (temperature > 99) {
+    tempWidth = 21 * size;
+  }
+
+  display.setCursor( ( ( width - headerWidth ) / 2) + x + 1, y );
+  display.setTextSize(1);
+  display.println("Temp:");
+
+  display.setTextSize(size);
+  display.setCursor( ( ( width - tempWidth ) / 2 ) + x + 1, y + HEADER_DATA_GAP );
+  String depthString =  String(temperature);
+  depthString.replace( '0', 'O' );
+
+  if (ferinheight ) {
+    depthString += String('F');
+  } 
+  else {
+    depthString += String('C');
+  }
+  display.print(depthString);
+
+  if ( bold ) {
+    display.setCursor( ( ( ( width - tempWidth ) / 2 ) + x) + 2, y + HEADER_DATA_GAP );
+    display.print(depthString);
+  }
+
+#ifdef SHOW_LAYOUT
+  display.drawRect(x, y, width, height, WHITE);
+#endif
+}
+
+// *******************************************************************
+// ************************** Draw Battery ***************************
+// *******************************************************************
+
+void drawBattery(int x, int y) {
+  int levelWidth = ( 24.0f / 255.0f ) * batteryLevel;
+  display.drawRect(x, y, 24, 8, WHITE);
+  display.fillRect(x, y, levelWidth, 8, WHITE);
+  display.drawLine(x + 24, y + 2, x + 24, y + 5, WHITE);
+}
+
+
+// *******************************************************************
 // *************************** Draw Depth ****************************
 // *******************************************************************
 
@@ -345,8 +623,8 @@ void drawAccentRate(void) {
   uint8_t color = WHITE;
   for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
     display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
+    display.width()/2-i, display.height()/2+i,
+    display.width()/2+i, display.height()/2+i, WHITE);
     if (color == WHITE) color = BLACK;
     else color = WHITE;
     display.display();
@@ -389,3 +667,111 @@ String secondsToString( unsigned long diveSeconds) {
 
   return timeString;
 }
+
+void showAlert(int time, String message) { // time = 0 will ask the user to dismiss. Time is in seconds
+
+  if (!alertShowing) {
+    alertShowing = true;
+    alertTime = now.unixtime();
+    display.clearDisplay();
+    display.display();
+
+    display.setTextSize(1);
+    display.setCursor( 0, 20 );
+    display.print(message);
+
+    if (time == 0) {
+      display.fillRect(115, 55, 13, 9, WHITE);
+      display.setTextColor(BLACK);
+      display.setCursor( 116, 56 );
+      display.setTextSize(1);
+      display.println("OK");
+    }
+
+    clockBlink = false;
+    if (clockBlink) {
+      display.fillRect(0, 0, 128, 18, BLACK);
+      display.setTextColor(WHITE);
+      display.setCursor( 35, 2 );
+      display.setTextSize(2);
+      display.println("ALERT:");
+    } 
+    else {
+      display.fillRect(0, 0, 128, 18, WHITE);
+      display.setTextColor(BLACK);
+      display.setCursor( 35, 2 );
+      display.setTextSize(2);
+      display.println("ALERT:");
+    } 
+    display.display();
+  }
+
+  PITimer1.period(1);
+  PITimer1.start(blinkAlert); 
+
+  if (time > 0) {
+    //    if (now.unixtime() > alertTime + time) {
+    //      display.clearDisplay();
+    //      display.display();
+    //      alertShowing = false;
+    //    }  
+    PITimer0.period(time);
+    PITimer0.start(killAlert); 
+  }
+}
+
+void killAlert() {
+  alertShowing = false;
+  PITimer1.stop();
+  PITimer0.stop();
+  display.clearDisplay();
+  display.display();
+}
+
+void blinkAlert() {
+  PITimer1.clear();
+  if (now.unixtime() > lastColonStateChange) {
+    lastColonStateChange = now.unixtime();
+    clockBlink = !clockBlink;
+  }
+
+  if (clockBlink) {
+    display.fillRect(0, 0, 128, 18, BLACK);
+    display.setTextColor(WHITE);
+    display.setCursor( 35, 2 );
+    display.setTextSize(2);
+    display.println("ALERT:");
+  } 
+  else {
+    display.fillRect(0, 0, 128, 18, WHITE);
+    display.setTextColor(BLACK);
+    display.setCursor( 35, 2 );
+    display.setTextSize(2);
+    display.println("ALERT:");
+  }
+  display.display();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
